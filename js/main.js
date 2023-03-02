a633fd31d28e08a649c7c565d399082450d0f0d8
Tran Tien <tientran0963598@gmail.com>
2023-03-02 14:35:54 +0700

    Handle Event Click for cell

--- js/main.js
+++ js/main.js
@@ -4,14 +4,11 @@ import {
   getCurrentTurnElement,
   getGameStatusElement,
 } from "./selectors.js";
-console.log(getCellElementList());
-console.log(getCellElementAtIdx(1));
-console.log(getCurrentTurnElement());
-console.log(getGameStatusElement());
+import { TURN } from "./constants.js";
 /**
  * Global variables
  */
-let currentTurn = "cross";
+let currentTurn = TURN.CROSS;
 let isGameEnded = false;
 let cellValues = new Array(9).fill("");
 
@@ -30,3 +27,29 @@ let cellValues = new Array(9).fill("");
  * 4. On replay button click --> reset game to play again.
  *
  */
+// 1. Bind click event for all cells
+function toggleCurrentTurn() {
+  // changCurrentTurn
+  currentTurn = currentTurn === TURN.CROSS ? TURN.CIRCLE : TURN.CROSS;
+  // Updata CurrentTurn Element in DOM
+  const currentTurnElement = getCurrentTurnElement();
+  currentTurnElement.classList.remove(TURN.CROSS, TURN.CIRCLE);
+  currentTurnElement.classList.add(currentTurn);
+}
+function handleCellClick(cell, index) {
+  // Check class cell
+  if (cell.className === TURN.CROSS || cell.className === TURN.CIRCLE) return;
+  //   set cross || circle for cell
+  cell.classList.add(currentTurn);
+  //   toggle Current Turn
+  toggleCurrentTurn();
+}
+function InitCellElementList() {
+  const cellElementList = getCellElementList();
+  cellElementList.forEach((cell, index) => {
+    cell.addEventListener("click", () => handleCellClick(cell, index));
+  });
+}
+(() => {
+  InitCellElementList();
+})();
--- js/selectors.js
+++ js/selectors.js
@@ -7,7 +7,7 @@ export function getCellElementList() {
   return document.querySelectorAll("#cellList li");
 }
 export function getCurrentTurnElement() {
-  return document.querySelector(".current-turn");
+  return document.querySelector("#currentTurn");
 }
 export function getCellElementAtIdx(index) {
   return document.querySelector(`#cellList > li:nth-child(${index + 1})`);
